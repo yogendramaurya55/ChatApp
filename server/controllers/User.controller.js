991dd7dae07f217f89df3218183538982e3a31af
yogendramaurya55 <yogendramaurya9083@gmail.com>
2025-11-29 02:56:37 +0530

    added models and controller

---
+++ server/controllers/User.controller.js
@@ -0,0 +1,111 @@
+import generateToken from "../lib/utils.js";
+import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+
+
+//signup function
+const signup = async (req , res)=>{
+    const {fullName , email , password , bio} = req.body;
+
+    try {
+        if(!fullName || ! !email || !password || !bio){
+            return res.json(
+                {
+                    success: false,
+                    message: "Missing Details"
+                }
+            )
+        }
+        const user = await User.findOne({email});
+        if(user){
+            return res.json({
+                success: false,
+                message: "Account already exist"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt);
+
+        const newUser = await User.create(
+            {
+                fullName,
+                email,
+                password: hashedPassword,
+                bio
+            }
+        );
+
+        const token = generateToken(newUser._id)
+
+        return res.json({
+            success: true,
+            userData: newUser,
+            token,
+            message: "Account created Successfully"
+        })
+    } catch (error) {
+        console.log(error.message)
+        return res.json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+
+//login function
+const login = async (req , res)=>{
+    try {
+        const {email , password} = req.body;
+
+        if(!email || !password) {
+            return res.json({
+                success: false,
+                message: "Missing Details"
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.json({
+                success: false,
+                message: "Account do not exist"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password , user.password)
+
+        if(!isPasswordCorrect){
+            return res.json({
+                success: false,
+                message: "Wrong credentials"
+            })
+        }
+
+        const token = generateToken(user._id);
+
+        const {hashedPassword , ...userData } = user.toObject();
+
+        return res.json({
+            success: true,
+            userData: userData,
+            token,
+            message: "Logged in succesfully"
+        })
+
+    } catch (error) {
+        console.log(error.message);
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+export 
+{
+    signup,
+    login
+}
